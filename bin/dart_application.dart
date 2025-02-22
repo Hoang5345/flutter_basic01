61783401e232519afbcc7874dc14061cd8fccecb
Hoang5357 <ht4727430@gmail.com>
2025-02-22 21:40:08 +0700

    hoangtv

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -3,21 +3,38 @@ class SinhVien {
   String? ten;
   int? tuoi;
   double? diemTrungBinh;
+
   SinhVien(this.maSinhVien, this.ten, this.tuoi, this.diemTrungBinh);
+
   void inThongTin() {
-    print(' $maSinhVien-$ten-$tuoi-$diemTrungBinh');
+    print('$maSinhVien - $ten - $tuoi - $diemTrungBinh');
+  }
+
+  bool duocHocBong() {
+    return diemTrungBinh! > 8.0;
+  }
+
+  void nhanHocBong() {
+    print('$maSinhVien - $ten - $diemTrungBinh');
   }
 }
 
 void main() {
-  List<SinhVien> danhSach = [];
-  SinhVien svHoang = SinhVien('SV001', "tran viet hoang", 18, 7.9);
-  danhSach.add(svHoang);
-  SinhVien svHai = SinhVien('SV002', 'dam duc hai', 18, 8.9);
-  danhSach.add(svHai);
-  SinhVien svPhong = SinhVien('SV003', 'nguyen thanh phong', 18, 9.6);
-  danhSach.add(svPhong);
+  List<SinhVien> danhSach = [
+    SinhVien('SV001', "Trần Việt Hoàng", 18, 7.9),
+    SinhVien('SV002', 'Đàm Đức Hải', 18, 8.9),
+    SinhVien('SV003', 'Nguyễn Thành Phong', 18, 9.6),
+  ];
+
+  print("Danh sách sinh viên:");
   for (var sv in danhSach) {
     sv.inThongTin();
+  }
+
+  print("\nSinh viên đủ điều kiện nhận học bổng:");
+  for (var sv in danhSach) {
+    if (sv.duocHocBong()) {
+      sv.nhanHocBong();
+    }
   }
 }
